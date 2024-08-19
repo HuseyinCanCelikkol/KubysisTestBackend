3bf7f4f75cc6e37b0565250e8ff06f63871c91e7
huseaky <155255751+huseaky@users.noreply.github.com>
2024-08-19 23:12:57 +0300

    user controller.cs de posttakileri sildim

--- BusinessLayer/Controllers/UserManagement/UserController.cs
+++ BusinessLayer/Controllers/UserManagement/UserController.cs
@@ -1,27 +1,12 @@
 ﻿using EntityLayer;
-using EntityLayer.Entities.UserManagement;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BusinessLayer.Controllers.UserManagement
 {
-	[Route("[controller]/[action]")]
+    [Route("[controller]/[action]")]
 	[ApiController]
 	public class UserController(KubysisDbContext kubysisDbContext) : ControllerBase
 	{
-		private readonly KubysisDbContext _kubysisDbContext = kubysisDbContext;
-
-		[HttpPost]
-
-		public async Task<IActionResult> AddUser()
-		{
-			try
-			{
-				_kubysisDbContext.
-			}
-			catch(Exception ex) 
-			{
-				return Problem(statusCode: 500,detail: ex.Message);
-			}
-		} 
+		private readonly KubysisDbContext _kubysisDbContext = kubysisDbContext;				
 	}
 }
