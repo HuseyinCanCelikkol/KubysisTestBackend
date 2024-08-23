e5fd47a4b5ce38f87b87b7a4189db072b55ae80d
huseaky <155255751+huseaky@users.noreply.github.com>
2024-08-24 01:21:17 +0300

    enumlar eklendi donation tablosu oluşturluor

---
+++ Common/Enums/DonationManagement/DonationClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Enums.DonationManagement
+{
+    public enum  DonationClass
+    {
+        KüçükBas=1,
+        Keçi,
+        BüyükBas,
+        HisseliSuKuyusu,
+        İsmeÖzelSuKuyusu,
+        KuranıKerim,
+        Ekmek,
+        GazzeYardımı,
+        SıcakYemek,
+        Pasta,
+        SutKeçisi,
+        KuranOkutma,
+        MutlulukPaketi,
+        YetimGiydirme,
+        UnDağıtımı,
+        YetimHamiliği,
+        ArkanBambuEv,
+        CamıMescit,
+        EğitimYardımı,
+        GıdaKolisi,
+        Diğer,
+        Diğer2,
+        Diğer3,
+        Diğer4,
+        Diğer5,
+    }
+}
